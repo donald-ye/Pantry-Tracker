4e1028b21a1dd327580f82e9ce10b6c73eef456a
Donald Ye <donaldye@Donalds-Air.lan>
2024-08-04 17:22:29 -0400

    fixes

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -19,8 +19,17 @@ const firebaseConfig = {
   
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const firestore = getFirestore(app);
+let firebaseApp;
+let firestore;
+let auth;
 
-export {firestore}
+const initializeFirebase = () => {
+    if (typeof window !== 'undefined' && !firebaseApp) {
+        firebaseApp = initializeApp(firebaseConfig);
+        firestore = getFirestore(firebaseApp);
+        auth = getAuth(firebaseApp);
+    }
+    return { firebaseApp, firestore, auth };
+};
+
+export { initializeFirebase };
